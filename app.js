bfb55a2fe5c9e49421b5fbdc08363b12faee76f8
Kaleb <kaleb.cpu@gmail.com>
2023-05-12 00:47:19 -0600

    scroll reveal uncomment

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ ScrollReveal({
     delay: 50
  });
 
-//  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-//  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-//  ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-//  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //  typed js
 
