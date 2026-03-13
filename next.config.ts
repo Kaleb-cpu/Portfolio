f55f72def0c31be4cb895664d2c938b80ecc7469
Kaleb Berhane <kaleb.cpu@gmail.com>
2026-03-12 22:52:18 -0700

    project tailwindcss bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  turbopack: {
+    root: __dirname,
+  },
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
